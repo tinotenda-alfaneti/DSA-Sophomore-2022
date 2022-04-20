3655e0ae03738acae842c0957d20f28bde33261d
tinotenda-alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2022-04-20 19:29:51 +0000

    Validate Binary Search Tree problem

---
+++ DataStructures/ValidateBST.java
@@ -0,0 +1,35 @@
+package DataStructures;
+
+public class ValidateBST {
+
+    
+     //Definition for a binary tree node.
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isValidBST(TreeNode root) {
+        
+        if (root == null) return true;
+        
+        return checkBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        
+        
+    }
+
+    boolean checkBST(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val <= min || root.val >= max) return false;
+        return checkBST(root.left, min, root.val) && checkBST(root.right, root.val, max);
+    }
+
+    
+}
